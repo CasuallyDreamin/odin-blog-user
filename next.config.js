de5defe29e80270c189912ebc468fe7f441e73ca
CasuallyDreamin <ysn.arambash@gmail.com>
2026-01-06 08:30:34 -0500

    conf: cloudflare next.config.js update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   webpack(config, { dev, isServer }) {
     const cssModuleRule = config.module.rules.find(
       (r) => r.oneOf
