92ebf93f1d49ce04fbabfa12abc8a8cbe4f2f8a3
CasuallyDreamin <ysn.arambash@gmail.com>
2026-01-07 08:19:10 -0500

    bugfix: import path fixes

--- src/app/components/home/ActivitySection.jsx
+++ src/app/components/home/ActivitySection.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import PostCard from './posts/PostCard';
+import PostCard from '../posts/PostCard';
 
 export default function ActivitySection({ posts = [], onPostClick }) {
   return (
--- src/app/components/home/HeroSection.jsx
+++ src/app/components/home/HeroSection.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { heroFade, idlePulse } from '../motion/home.motion';
+import { heroFade, idlePulse } from './home.motion';
 import '../styles/components/HeroSection.tailwind.css';
 
 export default function HeroSection({ title = 'SINTOPIA', subtitle = 'Coding, writing, and chaos' }) {
